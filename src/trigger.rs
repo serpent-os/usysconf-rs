ab7f8bdf21d002749f053b85e2cde790649de7c6
Fabio Forni <development@redaril.me>
2023-09-14 09:08:14 +0200

    trigger: Run tasks

--- src/trigger.rs
+++ src/trigger.rs
@@ -1,5 +1,6 @@
 use serde::Deserialize;
-use thiserror::Error;
+use std::io;
+use std::process::{Child, Command};
 
 /// Env describes a special status the operating system
 /// may be in. Some Triggers are not allowed to run in certain
@@ -10,11 +11,11 @@ pub enum Env {
     Live,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Default, Deserialize)]
 /// Task is a work unit for a Trigger.
 pub struct Task {
     /// Textual description of what this task does.
-    description: String,
+    pub description: String,
 
     /// Absolute path of the binary file called.
     binary: String,
@@ -23,13 +24,20 @@ pub struct Task {
     args: Vec<String>,
 }
 
+impl Task {
+    /// Runs this task in the background and returns a handle to it.
+    pub fn run(&self) -> io::Result<Child> {
+        Command::new(&self.binary).args(&self.args).spawn()
+    }
+}
+
 /// Trigger is a set of rules associated to a a list of paths.
 /// If allowed to run, Trigger will perform a set of tasks in response
 /// to the modified paths.
 #[derive(Debug, Deserialize)]
 pub struct Trigger {
     /// Lists of paths associated to this trigger.
-    paths: Vec<String>,
+    pub paths: Vec<String>,
 
     /// List of environments in which this trigger won't run.
     skip_envs: Vec<Env>,
@@ -37,9 +45,10 @@ pub struct Trigger {
     /// List of trigger names to be run before this trigger.
     deps: Vec<String>,
 
+    /// Whether tasks are independent from each other and
+    /// can be run concurrently.
+    concurrent: bool,
+
     /// Tasks that this trigger can perform.
     tasks: Vec<Task>,
 }
-
-#[derive(Error, Debug)]
-pub enum Error {}
